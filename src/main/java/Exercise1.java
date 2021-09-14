66ad54e7c22301a40e85672cf511b61f8de6391a
Tiffany Thani <tiffanythani18@gmail.com>
2021-09-13 23:01:28 -0400

    Initial commit

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Tiffany Thani
+ */
+
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args){
+        String  name;
+        Scanner scan = new Scanner (System.in);
+        System.out.print("What is your name?");
+        name = scan.next();
+        System.out.print("Hello, "  +name+ ", nice to meet you!");
+
+    }
+
+}
+
+
